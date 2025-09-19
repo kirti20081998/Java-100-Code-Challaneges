ebe780d2a57973bf00f8cb5298db90565542f166
Kirti Biswas <kirtibiswas04@gmail.com>
2025-09-19 23:31:22 +0530

    New code upload

--- practice/src/mathProgram/codeThird.java
+++ practice/src/mathProgram/codeThird.java
@@ -4,7 +4,7 @@
 
 package mathProgram;
 
-public class javaThird {
+public class codeThird {
 	
 	public int singleNumber(int[] nums) {
         int result = 0;
@@ -16,7 +16,7 @@ public class javaThird {
 
     // Example usage
     public static void main(String[] args) {
-    	javaThird solution = new javaThird();
+    	codeThird solution = new codeThird();
 
         int[] nums1 = {2, 2, 1};
         System.out.println("Single number: " + solution.singleNumber(nums1)); // 1
